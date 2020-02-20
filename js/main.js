d3a6607e688d44af66c2468948269eac4874a785
JAlvarez9118 <AlvarezLJose@yahoo.com>
2020-02-20 10:01:45 -0500

    Commit 7 - completed the restaurant landing page. Started adding Javascript logic for the slider section

--- js/main.js
+++ js/main.js
@@ -1,12 +1,52 @@
-// import "css/animate.scss";
-// $('.food-image').hover(
-//     function () { $(this).toggleClass('animated jello') },
-//     function () { $(this).removeClass('animated jello') }
-// )
+var r = -20;
+var l = +20;
+
+var shiftR = `${r}%, 0, 0`;
+
+var shiftL = "-0%, 0, 0";
+
+var slideStyle = document.getElementById("slideSystem").style.transform;
 
 
-$(document).ready(function () {
-    $(".food-image").hover(function () {
-        $(this).addClass("animated jello");
-    });
+// RIGHT SLIDE MOVE
+var right = document.getElementById("rightBtn").addEventListener("click", () => {
+
+    document.getElementById("slideSystem").style.transform = `translate3d(${shiftR})`;
+    console.log(r);
+    var a = 20;
+    shiftR = `$({r} (- a))%, 0, 0`
+
+    console.log(shiftR);
+    console.log(r);
 });
+
+
+
+// LEFT SLIDE MOVE
+var left = document.getElementById("leftBtn").addEventListener("click", () => {
+    document.getElementById("slideSystem").style.transform = `translate3d( ${shiftL})`;
+});
+
+
+
+
+
+
+
+
+
+
+// var right = document.getElementById("rightBtn").addEventListener("click", function () {
+//     document.getElementById("slideSystem").style.transform = "translate3d(-20%, 0, 0)"
+// });
+
+
+
+// slide left by 1 slide (20%)
+// function move() {
+//     document.getElementById("slideSystem").style.transform = "translate3d(-20%, 0, 0)";
+// };
+
+// var let = () => {
+
+// }
